9e27bb74ca1b5eccd365aac374db80c8b94c73d7
Hashim <hashbash2299@gmail.com>
2026-09-24 02:03:23 -0700

    update

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 import { useState, useEffect, useRef } from "react";
 import { db, auth } from "../lib/firebase";
 import { collection, query, where, onSnapshot, doc, addDoc, serverTimestamp, orderBy, updateDoc, setDoc, getDoc, deleteDoc } from "firebase/firestore";
